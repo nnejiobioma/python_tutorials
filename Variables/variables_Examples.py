abf25d82721be6bb3225fb62ec3d42a596acdda8
Nneji Obioma <nnejiobioma@gmail.com>
2024-02-15 20:35:18 +0100

    Varibles folder created

---
+++ Variables/variables_Examples.py
@@ -0,0 +1,62 @@
+#creating variable integer variable
+b = 10
+print(b)
+# to check which type of variable
+print (type(b))
+
+#This will print 10 as output when you run the code python variables.py
+
+#----------------------------------------------------------------------
+#Creating a float variable
+b = 10.5
+print(b)
+# to check which type of variable
+print (type(b))
+
+#This will print 10.5 as an output and also type 'str' as data type
+#----------------------------------------------------------------------
+#creating a sting vatiable
+b = "gate"
+print(b)
+# to check which type of variable
+print (type(b))
+
+#-----------------------------------------------------------------------
+#Creating a list variable
+b = [1,2,3,4,5,6,7]
+print(b)
+# to check which type of variable
+print (type(b))
+
+#------------------------------------------------------------------------
+#Create a set of variables
+b = {'dog','cat','goat'}
+print(b)
+# to check which type of variable
+print (type(b))
+
+#-----------------------------------------------------------------------
+#Create a turple variable
+b = (1,2,4,4,3,5)
+print(b)
+# to check which type of variable
+print (type(b))
+
+
+#TYPE CONVERSION or TYPE CASTING
+b = (input("Enter an alphabeth : "))
+print("Alphbeth : ", b)
+# to check which type of variable
+print (type(b))
+
+#To convert the data type use int
+
+b = int(input("Enter an alphabeth : "))
+print("Alphbeth : ", b)
+# to check which type of variable
+print (type(b))
+
+#from this example, the initial data type will be converted depending on the input data type
+#note that string data types cannot be converted
+
+#----------------------------------------------------------------------
